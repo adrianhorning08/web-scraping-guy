6f2217d23147756272947210255696098c9024e6
Adrian Horning <m.adrian.horning@gmail.com>
2023-07-05 11:41:28 -0500

    better color on inputs

--- src/components/ApiDemos.jsx
+++ src/components/ApiDemos.jsx
@@ -365,7 +365,7 @@ export default function ApiDemos() {
             <input
               id="parcelNumber"
               type="text"
-              className="mt-4 block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+              className="mt-4 block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6 bg-stone-600"
               placeholder="2004014001"
               value={parcelId}
               onChange={(e) => setParcelId(e.target.value)}
@@ -428,7 +428,7 @@ export default function ApiDemos() {
             </label>
             <select
               id="docType"
-              className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-400 sm:text-sm sm:leading-6"
+              className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-400 sm:text-sm sm:leading-6 bg-stone-600"
               value={docType}
               onChange={(e) => setDocType(e.target.value)}
             >
@@ -436,7 +436,7 @@ export default function ApiDemos() {
             </select>
             <input
               type="text"
-              className="mt-4 block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+              className="mt-4 block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6 bg-stone-600"
               placeholder="Search Term"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -16,7 +16,7 @@ export default function NumberInput({
       <input
         id={id}
         type="number"
-        className="mb-2 block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+        className="mb-2 block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6 bg-stone-600"
         placeholder={placeholder}
         value={value}
         onChange={onChange}
